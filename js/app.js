99c6a011b30321c442521d41965c1fee1d5c1679
Nikolay <Koliansssss2014@mail.ru>
2023-07-13 16:30:58 +0300

    Animation elements

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ const accordion = document.querySelector('.accordion'),
         accordionItem.forEach(item => {
             item.lastElementChild.classList.add('hide');
             item.lastElementChild.classList.remove('show');
+            item.lastElementChild.lastElementChild.classList.add('hideContent');
+            item.lastElementChild.lastElementChild.classList.remove('showContent');
             item.firstElementChild.lastElementChild.lastElementChild.classList.remove('rotateImg')
         })
       }
@@ -15,8 +17,9 @@ const accordion = document.querySelector('.accordion'),
       function accordionContentShow(i=0){
         accordionItem[i].lastElementChild.classList.add('show');
         accordionItem[i].lastElementChild.classList.remove('hide');
-        accordionItem[i].firstElementChild;
-        accordionItem[i].firstElementChild.lastElementChild.lastElementChild.classList.add('rotateImg')
+        accordionItem[i].lastElementChild.lastElementChild.classList.add('showContent');
+        accordionItem[i].lastElementChild.lastElementChild.classList.remove('hideContent');
+        accordionItem[i].firstElementChild.lastElementChild.lastElementChild.classList.add('rotateImg');
       }
 
     accordion.addEventListener('click', (ev) => {
@@ -118,41 +121,126 @@ const accordion = document.querySelector('.accordion'),
           arrow_up = document.querySelector('.arrow_up'),
           arrow_down = document.querySelector('.arrow_down');
           
-    console.dir(slide);
     let firstSlide = 0;
     let secondSlide = 1;
 
 function showSlide(firstIndex, secondIndex) {
-  slides.forEach((slide) => {
+    slides.forEach((slide) => {
     slide.classList.remove('active');
-  });
-  slides[firstIndex].classList.add('active');
-  slides[secondIndex].classList.add('active');
+    });
+    slides[firstIndex].classList.add('active');
+    slides[secondIndex].classList.add('active');
 }
 
 function showNextSlide() {
     firstSlide = firstSlide + 2;
     secondSlide = secondSlide + 2;
-  console.log(firstSlide);
-  console.log(secondSlide);
     if (firstSlide >= slides.length) {
         firstSlide = 0;
         secondSlide = 1;
     }
     showSlide(firstSlide, secondSlide);
+    ul.classList.remove('animated_slide_up');
+    ul.classList.remove('animated_slide_down');
+    setTimeout(() => {
+        ul.classList.add('animated_slide_down');
+    }, 0)
 }
 
 function showPreviousSlide() {
     firstSlide = firstSlide - 2;
     secondSlide = secondSlide - 2;
-  if (firstSlide < 0) {
-    firstSlide = slides.length - 1;
-    secondSlide = slides.length - 2;
-  }
-  showSlide(firstSlide, secondSlide);
+    if (firstSlide < 0) {
+        firstSlide = slides.length - 1;
+        secondSlide = slides.length - 2;
+    }
+    showSlide(firstSlide, secondSlide);
+    ul.classList.remove('animated_slide_up');
+    ul.classList.remove('animated_slide_down');
+    setTimeout(() => {
+        ul.classList.add('animated_slide_up');
+    }, 0)
 }
 
 arrow_up.addEventListener('click', showPreviousSlide);
 arrow_down.addEventListener('click', showNextSlide);
 
 showSlide(firstSlide, secondSlide);
+
+const blockWithButton = document.querySelector('.titel_block'),
+      btn = blockWithButton.querySelector('button');
+
+      function pauseAnimation() {
+        btn.style.animationPlayState = 'paused';
+      }
+      
+      function resumeAnimation() {
+        btn.style.animationPlayState = 'running';
+      }
+      
+      btn.addEventListener('mouseover', pauseAnimation);
+      btn.addEventListener('mouseout', resumeAnimation);
+
+
+const img = document.querySelectorAll('.img'),
+      person_image = document.querySelectorAll('.person_image'),
+      personBoxImage = document.querySelectorAll('.person_box_image'),
+      contentComment = document.querySelectorAll('.person_content_comment'),
+      commentList = document.querySelector('.persan_comment_list');
+
+function hideTabContent(){
+    contentComment.forEach(item => {
+        item.classList.add('person_hide');
+        item.classList.remove('person_show');
+    });
+    img.forEach(item => {
+        item.classList.remove('image_active');
+    });
+    person_image.forEach(item => {
+        item.classList.remove('boxShadowInset');
+    });
+    personBoxImage.forEach(item => {
+        item.classList.remove('boxShadow');
+        item.classList.add('fadeOut');
+        item.classList.remove('fadeIn');
+    });
+}
+hideTabContent();
+
+function hideBlockTitleAndComment(){
+    contentComment.forEach(item => {
+        item.firstElementChild.classList.add('titel_hide');
+        item.firstElementChild.classList.remove('titel_show');
+        item.lastElementChild.classList.add('description_hide');
+        item.lastElementChild.classList.remove('description_show');
+    });
+    personBoxImage.forEach(item => {
+        item.classList.add('fadeOut');
+        item.classList.remove('fadeIn');
+    });
+} 
+
+function showTabContent(i = 0){
+    contentComment[i].classList.add('person_show');
+    contentComment[i].classList.remove('person_hide');
+    contentComment[i].firstElementChild.classList.add('titel_show');
+    contentComment[i].firstElementChild.classList.remove('titel_hide');
+    contentComment[i].lastElementChild.classList.add('description_show');
+    contentComment[i].lastElementChild.classList.remove('description_hide');
+    img[i].classList.add('image_active');
+    person_image[i].classList.add('boxShadowInset');
+    personBoxImage[i].classList.add('boxShadow');
+    personBoxImage[i].classList.add('fadeIn');
+    personBoxImage[i].classList.remove('fadeOut');
+}
+showTabContent();
+let i = 1;
+setInterval(() => {
+    (i === 3)? i=0 : i;
+    hideTabContent();
+    setTimeout(()=>{
+        hideBlockTitleAndComment();
+    },9500);
+    showTabContent(i);
+    i++; 
+  }, 10000)
